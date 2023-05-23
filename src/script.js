345458dc60cdc9bf2fec2f350d1d8f56162a4e9c
Hardik <singhhardik6014@gmail.com>
2023-05-23 19:30:22 +0530

    desktop ver done

--- src/script.js
+++ src/script.js
@@ -1,6 +1,11 @@
 const navbar = document.getElementById('pop')
 let menu = document.getElementById('Menu')
 const close = document.getElementById('close')
+let product = document.getElementById('product')
+
+product.addEventListener('click', () => {
+    navbar.style.display = 'block';
+})
 
 menu.addEventListener('click', () => {
     navbar.style.display = 'block';
